e68911ed1a2fca5354e955273180cc5d43735c8b
Gunal-k <gunalk.0007@gmail.com>
2025-01-30 22:55:14 +0530

    Implement TwoSum algorithm with example usage

--- TwoSum.py
+++ TwoSum.py
@@ -1,1 +1,13 @@
- 
+class solution:
+    def twoSum(self,nums:list[int],target:int)->list[int]:
+        hashmap = {} # key: value, value: index
+        for i , n in enumerate(nums):
+            diff = target - n
+            if diff in hashmap:
+                return [hashmap[diff], i ]
+            hashmap[n]=i
+        return
+
+# Create an instance of the Solution class
+sol = solution()
+print(sol.twoSum([2,1,5,3],4))
