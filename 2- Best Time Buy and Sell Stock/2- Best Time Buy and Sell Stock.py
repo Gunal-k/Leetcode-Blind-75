b4904bf2ffc18fbfa29ec6dbe2d7695554ecbb5e
Gunal-k <gunalk.0007@gmail.com>
2025-01-31 21:10:05 +0530

    Add implementation of Best Time to Buy and Sell Stock algorithm

---
+++ 2- Best Time Buy and Sell Stock/2- Best Time Buy and Sell Stock.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxStock(self,prices : list[int])->int:
+        lPointer, rPointer = 0, 1
+        maxProfit = 0
+        while rPointer < len(prices):
+            if prices[lPointer] < prices[rPointer]:
+                profit = prices[rPointer] - prices[lPointer]
+                maxProfit = max(maxProfit, profit)
+            else:
+                lPointer += 1
+            rPointer += 1
+        return maxProfit
+sol = Solution()
+print(sol.maxStock([7,1,5,3,6,4]))
