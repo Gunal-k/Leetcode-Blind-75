f485270ffc0e755093d759ce369c8ab875a69a32
Gunal-k <gunalk.0007@gmail.com>
2025-03-06 21:32:59 +0530

    updated code

--- 50- Longest Repeating Character Replacement/50-LongestRepeatingCharacterReplacement.py
+++ 50- Longest Repeating Character Replacement/50-LongestRepeatingCharacterReplacement.py
@@ -4,27 +4,32 @@ class Solution:
         res = l = 0
 
         for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r],0)
+            count[s[r]] = 1 + count.get(s[r], 0)
 
-            while (r-l+1) - max(count.values()) > k:
-                count[s[l]] -=1
-                l+=1
-            
-            res = max(res, r-l+1)
-        return res
-                       #Or
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        maxf = res = l = 0
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
 
-        for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r],0)
-            maxf = max(maxf,count[s[r]])
-            while (r-l+1) - maxf > k:
-                count[s[l]] -=1
-                l+=1
-            
-            res = max(res, r-l+1)
+            res = max(res, r - l + 1)
         return res
+
+# Example usage:
+if __name__ == "__main__":
+    solution = Solution()
+    s = "AABABBA"
+    k = 1
+    print(solution.characterReplacement(s, k))  # Output: 4
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+#         count = {}
+#         maxf = res = l = 0
+
+#         for r in range(len(s)):
+#             count[s[r]] = 1 + count.get(s[r],0)
+#             maxf = max(maxf,count[s[r]])
+#             while (r-l+1) - maxf > k:
+#                 count[s[l]] -=1
+#                 l+=1
             
+#             res = max(res, r-l+1)
+#         return res
